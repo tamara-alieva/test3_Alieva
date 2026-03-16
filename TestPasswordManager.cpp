f912244f84acd848d81fcf06e109074e6104cb67
Tamara <tamaraavgust14@gmail.com>
2026-03-17 00:03:56 +0700

    Тест addCard() для Sberbank

--- TestPasswordManager.cpp
+++ TestPasswordManager.cpp
@@ -25,3 +25,13 @@ TEST_CASE("Add Google website credentials test") {
 	REQUIRE(manager.websites["Google"].login == "user@gmail.com");
 	REQUIRE(manager.websites["Google"].password == "123456");
 }
+
+TEST_CASE("Add Sberbank card credentials test") {
+	PasswordManager manager;
+	CardCredentials cardData{ "Sberbank", "1234 5678 9012 3456", "08/28", "123"};
+	manager.addCard(cardData);
+	REQUIRE(manager.cards["Sberbank"].bank == "Sberbank");
+	REQUIRE(manager.cards["Sberbank"].number == "1234 5678 9012 3456");
+	REQUIRE(manager.cards["Sberbank"].expirationDate == "08/28");
+	REQUIRE(manager.cards["Sberbank"].cvcCode == "123");
+}
