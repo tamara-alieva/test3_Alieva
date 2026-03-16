0e1f7d6a565c7e394f0f145e6ab514074e85c598
Tamara <tamaraavgust14@gmail.com>
2026-03-16 22:52:06 +0700

    Добавлена упрощенная реализация addWebsite()

--- PasswordManager.cpp
+++ PasswordManager.cpp
@@ -2,3 +2,9 @@
 
 PasswordManager::PasswordManager() {}
 
+void PasswordManager::addWebsite(WebsiteCredentials siteData) {
+	// to do реализовать метод
+	WebsiteCredentials cred{ "Yandex", "user@yandex.ru", "password123" };
+	websites["Yandex"] = cred;
+}
+
--- PasswordManager.h
+++ PasswordManager.h
@@ -1,7 +1,18 @@
 #pragma once
+#include <map>
+#include <string>
+using namespace std;
+
+struct WebsiteCredentials {
+	string siteName;
+	string login;
+	string password;
+};
 
 class PasswordManager {
 public:
+	map<string, WebsiteCredentials> websites;
 	PasswordManager();
+	void addWebsite(WebsiteCredentials siteData);
 };
 
