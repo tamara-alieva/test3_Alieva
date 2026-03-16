75cbdd5b66c582421a047b402d342d01cfa88a4c
Tamara <tamaraavgust14@gmail.com>
2026-03-16 22:40:29 +0700

    Тест addWebsite() для Yandex

--- TestPasswordManager.cpp
+++ TestPasswordManager.cpp
@@ -8,3 +8,11 @@ TEST_CASE("PasswordManager creation test") {
 	REQUIRE(true);
 }
 
+TEST_CASE("Add Yandex website credentials test") {
+	PasswordManager manager;
+	WebsiteCredentials siteData{ "Yandex", "user@yandex.ru", "password123" };
+	manager.addWebsite(siteData);
+	REQUIRE(manager.websites["Yandex"].siteName == "Yandex");
+	REQUIRE(manager.websites["Yandex"].login == "user@yandex.ru");
+	REQUIRE(manager.websites["Yandex"].password == "password123");
+}
