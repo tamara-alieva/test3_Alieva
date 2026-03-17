d1eb8cd0bd100b42ede2802adeedb7a079490bf3
Tamara <tamaraavgust14@gmail.com>
2026-03-17 22:27:50 +0700

    Тест для getAllCredentials()

--- TestPasswordManager.cpp
+++ TestPasswordManager.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "PasswordManager.h"
+#include <vector>
 using namespace std;
 
 TEST_CASE("PasswordManager creation test") {
@@ -44,4 +45,36 @@ TEST_CASE("Add VTB card credentials test") {
 	REQUIRE(manager.cards["VTB"].number == "9876 5432 1098 7654");
 	REQUIRE(manager.cards["VTB"].expirationDate == "10/29");
 	REQUIRE(manager.cards["VTB"].cvcCode == "456");
+}
+
+TEST_CASE("Get all credentials test") {
+	PasswordManager manager;
+	WebsiteCredentials siteData1{ "Yandex", "user@yandex.ru", "password123" };
+	WebsiteCredentials siteData2{ "Google", "user@gmail.com", "123456" };
+	CardCredentials cardData1{ "Sberbank", "1234 5678 9012 3456", "08/28", "123" };
+	CardCredentials cardData2{ "VTB", "9876 5432 1098 7654", "10/29", "456" };
+	manager.addWebsite(siteData1);
+	manager.addWebsite(siteData2);
+	manager.addCard(cardData1);
+	manager.addCard(cardData1);
+	vector<UnifiedCredentials> result = manager.getAllCredentials();
+	REQUIRE(result.size() == 4);
+	bool foundYandex = false,
+		foundGoogle = false,
+		foundSberbank = false,
+		foundVTB = false;
+	for (int i = 0; i < result.size(); i++) {
+		if (result[i].type == CredType::WEBSITE && result[i].website.siteName == "Yandex")
+			foundYandex = true;
+		if (result[i].type == CredType::WEBSITE && result[i].website.siteName == "Google")
+			foundGoogle = true;
+		if (result[i].type == CredType::CARD && result[i].card.bank == "Sberbank")
+			foundSberbank = true;
+		if (result[i].type == CredType::CARD && result[i].card.bank == "VTB")
+			foundVTB = true;
+	}
+	REQUIRE(foundYandex);
+	REQUIRE(foundGoogle);
+	REQUIRE(foundSberbank);
+	REQUIRE(foundVTB);
 }
