89bc91ab642b468b61e19a74646aed5ce6e799a9
Tamara <tamaraavgust14@gmail.com>
2026-03-18 00:54:15 +0700

    Дополнен функционал приложения в main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ void printMenu() {
 	cout << "--- PASSWORD MANAGER ---" << endl;
 	cout << "1. Add website credentials" << endl;
 	cout << "2. Add card credentials" << endl;
-	cout << "3. Exit" << endl;
+	cout << "3. Show all credentials" << endl;
+	cout << "4. Search credentials" << endl;
+	cout << "5. Exit" << endl;
 	cout << "Choose option: ";
 }
 
@@ -37,26 +39,78 @@ void addCard(PasswordManager& manager) {
 	cout << cred.bank << " card credentials added!" << endl << endl;
 }
 
-int main() {
-	PasswordManager manager;
-	int option = 1;
-
-	while (true) {
-		printMenu();
-		cin >> option; cin.get();
-		switch (option) {
-		case 1:
-			addWebsite(manager);
-			break;
-		case 2:
-			addCard(manager);
-			break;
-		case 3:
-			cout << "Goodbye!" << endl;
-			return 0;
-		default:
-			cout << "Invalid option. Try again!" << endl << endl;
+void showAllCredentials(PasswordManager manager) {
+	vector<UnifiedCredentials> credentials = manager.getAllCredentials();
+	if (credentials.empty())
+		cout << "No credentials found!" << endl << endl;
+	else {
+		cout << "--- ALL CREDENTIALS ---" << endl;
+		for (int i = 0; i < credentials.size(); i++) {
+			if (credentials[i].type == CredType::WEBSITE) {
+				cout << "- Website " << credentials[i].website.siteName << endl
+					<< "\tLogin: " << credentials[i].website.login << endl
+					<< "\tPassword: " << credentials[i].website.password << endl << endl;
+			}
+			else if (credentials[i].type == CredType::CARD) {
+				cout << "- Card " << credentials[i].card.bank << endl
+					<< "\tNumber: " << credentials[i].card.number << endl
+					<< "\tExpiration date: " << credentials[i].card.expirationDate << endl
+					<< "\tCVC code: " << credentials[i].card.cvcCode << endl << endl;
+			}
 		}
 	}
-	return 0;
+}
+
+void searchCredentials(PasswordManager manager) {
+	string query;
+	cout << "- Enter search query: "; getline(cin, query);
+	vector<UnifiedCredentials> credentials = manager.searchCredentials(query);
+	if (credentials.empty())
+		cout << "No credentials found for query \"" << query << "\"!" << endl << endl;
+	else {
+		cout << "--- SEARCH RESULTS ---" << endl;
+		for (int i = 0; i < credentials.size(); i++) {
+			if (credentials[i].type == CredType::WEBSITE) {
+				cout << "- Website " << credentials[i].website.siteName << endl
+					<< "\tLogin: " << credentials[i].website.login << endl
+					<< "\tPassword: " << credentials[i].website.password << endl << endl;
+			}
+			else if (credentials[i].type == CredType::CARD) {
+				cout << "- Card " << credentials[i].card.bank << endl
+					<< "\tNumber: " << credentials[i].card.number << endl
+					<< "\tExpiration date: " << credentials[i].card.expirationDate << endl
+					<< "\tCVC code: " << credentials[i].card.cvcCode << endl << endl;
+			}
+		}
+	}
+}
+
+int main() {
+	PasswordManager manager;
+		int option = 1;
+
+		while (true) {
+			printMenu();
+			cin >> option; cin.get();
+			switch (option) {
+			case 1:
+				addWebsite(manager);
+				break;
+			case 2:
+				addCard(manager);
+				break;
+			case 3:
+				showAllCredentials(manager);
+				break;
+			case 4:
+				searchCredentials(manager);
+				break;
+			case 5:
+				cout << "Goodbye!" << endl;
+				return 0;
+			default:
+				cout << "Invalid option. Try again!" << endl << endl;
+			}
+		}
+		return 0;
 }
